b06305bb42a265815a6b717629249db1e5763d50
Kymberlly <kymberllyglaucia@gmail.com>
2021-05-15 16:42:39 -0300

    Criação endpoint de monitoramento

--- aplicacao.py
+++ aplicacao.py
@@ -1,2 +1,23 @@
-import Flask
+from flask import Flask
 
+aplicacao = Flask(__name__)
+
+
+@aplicacao.route('/monitoramento/<int:tempo_espera>', methods=['GET'])
+def monitoramento(tempo_espera):
+    import requests
+    import time
+
+    while True:
+        resposta_api = requests.get('https://poloniex.com/public?command=returnTicker')
+
+        if resposta_api.status_code != 200:
+            raise ValueError('Erro ao realizar chamda da API Poloniex.')
+
+        time.sleep(tempo_espera)
+
+    return "<h1>Monitoramento</h1>"
+
+
+aplicacao.config["DEBUG"] = True
+aplicacao.run()
